2f37b8d0231bacc65363ef343ad62debb22dd8dc
03Lucas <lucas.lopes401@hotmail.com>
2024-10-20 16:30:45 -0300

    Projeto criado

---
+++ backend/src/main/java/com/example/Bmovie/BmovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Bmovie/BmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
